d85503b5ec6d4a5bf64f0a3acde153c26a1f6318
Mo <mo.ghorb@gmail.com>
2025-05-27 20:14:57 +0330

    wip: update ExpressionAdmin and GrammarAdmin to include created_at and updated_at in list_display

--- english-assistant/expression/admin.py
+++ english-assistant/expression/admin.py
@@ -6,6 +6,6 @@ from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
 
 @admin.register(models.Expression)
 class ExpressionAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
-    list_display = ("pk", "title")
+    list_display = ("pk", "title", "created_at", "updated_at")
     search_fields = ("title",)
     list_filter = ("title",)
--- english-assistant/grammar/admin.py
+++ english-assistant/grammar/admin.py
@@ -6,6 +6,6 @@ from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
 
 @admin.register(models.Grammar)
 class GrammarAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
-    list_display = ("pk", "title")
+    list_display = ("pk", "title", "created_at", "updated_at")
     search_fields = ("title",)
     list_filter = ("title",)
