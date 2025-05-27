8f26fcb92c9c1ec98e14ee76fb7782d586d6f872
Mo <mo.ghorb@gmail.com>
2025-05-27 10:34:18 +0330

    feat: implement pagination for Grammar API endpoints

--- english-assistant/grammar/views.py
+++ english-assistant/grammar/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
 
 from . import models, serializers
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
 
 
 class GrammarViewSet(viewsets.ReadOnlyModelViewSet):
@@ -12,3 +19,4 @@ class GrammarViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Grammar.objects.filter(deleted_at__isnull=True)
     serializer_class = serializers.GrammarSerializer
     permission_classes = [AllowAny]
+    pagination_class = StandardResultsSetPagination
