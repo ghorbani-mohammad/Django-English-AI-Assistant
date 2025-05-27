6f62a1c9bef41cdd2740b8307926082bfa37941c
Mo <mo.ghorb@gmail.com>
2025-05-27 20:12:58 +0330

    feat: add ExpressionAdmin for managing Expression model in admin interface

--- english-assistant/expression/admin.py
+++ english-assistant/expression/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
+
+
+@admin.register(models.Expression)
+class ExpressionAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
+    list_display = ("pk", "title")
+    search_fields = ("title",)
+    list_filter = ("title",)
