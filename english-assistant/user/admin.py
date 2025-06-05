a779c5b8f4ae1471f23edaa343e33fd089f85620
Mo <mo.ghorb@gmail.com>
2025-06-05 08:51:52 +0330

    feat: add Profile model and admin interface for user management

--- english-assistant/user/admin.py
+++ english-assistant/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
+    list_display = ("pk", "user", "user__first_name", "user__last_name", "created_at", "updated_at")
+    search_fields = ("user__first_name", "user__last_name", "user__email")
--- english-assistant/user/models.py
+++ english-assistant/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from reusable.models import BaseModel
+
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"({self.pk} {self.user.first_name} {self.user.last_name})"
