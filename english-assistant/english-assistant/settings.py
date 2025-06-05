f101357f5c623dfb613a28865fd953626869d8fd
Mo <mo.ghorb@gmail.com>
2025-06-05 21:23:28 +0330

    fix: update EMAIL_HOST configuration to use environment variables

--- english-assistant/english-assistant/settings.py
+++ english-assistant/english-assistant/settings.py
@@ -158,7 +158,7 @@ CHANNEL_LAYERS = {
 # Email Configs
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp-mail.outlook.com"
+EMAIL_HOST = env("EMAIL_HOST", default=None)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default=None)
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default=None)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
