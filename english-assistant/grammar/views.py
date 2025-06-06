f57a2d6881e3a7e0abdd25358943a4d47be6beb0
Mo <mo.ghorb@gmail.com>
2025-06-06 23:05:32 +0330

    fix: change permission class in GrammarViewSet to require JWT authentication

--- english-assistant/grammar/views.py
+++ english-assistant/grammar/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 
 from . import models, serializers
@@ -15,9 +15,10 @@ class GrammarViewSet(viewsets.ReadOnlyModelViewSet):
     """
     A readonly viewset for viewing Grammar instances.
     Provides list and retrieve actions only.
+    Requires JWT authentication.
     """
 
     queryset = models.Grammar.objects.filter(deleted_at__isnull=True).order_by("-id")
     serializer_class = serializers.GrammarSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     pagination_class = StandardResultsSetPagination
